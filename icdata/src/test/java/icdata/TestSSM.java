b2b913f32f8ebaf444c6c2f3dda3ed301f0e65b7
ILMare <ILMare@192.168.1.101>
2018-10-31 00:32:13 +0800

    sms

--- icdata/src/test/java/icdata/TestSSM.java
+++ icdata/src/test/java/icdata/TestSSM.java
@@ -12,10 +12,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  
 import com.alibaba.fastjson.JSON;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
 import com.haixia.pojo.Permission;
 import com.haixia.pojo.Role;
 import com.haixia.pojo.User;
 import com.haixia.service.IUserService;
+import com.haixia.util.SMSUtil;
  
 @RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
@@ -34,16 +37,31 @@ public class TestSSM {
  
 	@Test
 	public void test1() {
-		User user = userService.getById(1);
-//		User user = userService.getByUserName("刘德华");
-		// System.out.println(user.getUserName());
-		for (Role role : user.getRoles()) {
-			logger.info(role.getRoleName());
-			for(Permission permission:role.getPermissions()) {
-				logger.info(permission.getPermissionName());
-			}
+		SMSUtil sms = new SMSUtil();
+		SendSmsResponse response;
+		try {
+			response = sms.sendSms();
+			System.out.println("短信接口返回的数据----------------");
+		    System.out.println("Code=" + response.getCode());
+		    System.out.println("Message=" + response.getMessage());
+		    System.out.println("RequestId=" + response.getRequestId());
+		    System.out.println("BizId=" + response.getBizId());
+		} catch (ClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		logger.info("getByUsername 值："+user.getUserName()+user.getPassword()+user.getUserName());
-		logger.info(JSON.toJSONString(user));
+	    
+		
+//		User user = userService.getById(1);
+////		User user = userService.getByUserName("刘德华");
+//		// System.out.println(user.getUserName());
+//		for (Role role : user.getRoles()) {
+//			logger.info(role.getRoleName());
+//			for(Permission permission:role.getPermissions()) {
+//				logger.info(permission.getPermissionName());
+//			}
+//		}
+//		logger.info("getByUsername 值："+user.getUserName()+user.getPassword()+user.getUserName());
+//		logger.info(JSON.toJSONString(user));
 	}
 }
